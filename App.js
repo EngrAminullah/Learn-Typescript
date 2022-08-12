6629f2bf433ea08e35b150ccb4e993987c8c92a0
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-12 12:00:13 +0500

    object

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 // export {}
-// class App{
-//     test(){
-//         console.log("Test function working")
-//     }
-// }
-// let a= new App();
-// a.test()
-// let a:number=10.10;
-// console.warn(a)
-var data = ['amin', 'bacha', 'sajid', 10, true];
-data.push("Mansoor");
-data[3] = true;
-console.log(data);
+var usersTyped = {
+    name: 'bruce',
+    age: 30,
+    address: "street 20,25 and others"
+};
+usersTyped.name = 'peter';
+console.warn(usersTyped);
--- App.ts
+++ App.ts
@@ -9,7 +9,22 @@
 // a.test()
 // let a:number=10.10;
 // console.warn(a)
-let data=['amin','bacha','sajid',10,true]
-data.push("Mansoor")
-data[3]=true
-console.log(data)
+// let data=['amin','bacha','sajid',10,true]
+// data.push("Mansoor")
+// data[3]=true
+// console.log(data)
+
+
+interface usersTyped{
+    name:string,
+    age:number,
+    address:any
+}
+
+let usersTyped={
+    name:'bruce',
+    age:30,
+    address:"street 20,25 and others"
+}
+usersTyped.name='peter'
+console.warn(usersTyped)
